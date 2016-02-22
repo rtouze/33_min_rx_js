19040ffac25044f7767258c2a370a04648f6d7f5
Romain TOUZE <romain.touze@gmail.com>
2016-02-22 17:10:13 +0100

    introduction de rxjs

--- shortcuts.js
+++ shortcuts.js
@@ -1,14 +1,16 @@
 $(function() {
     console.log("c'est parti!");
     const $result = $('#result');
-    document.body.onkeyup = (event) => {
-        console.log(event.key);
-        if (event.key === "f") {
-           requestFullScreen();
-        }
-        $result.html(`Touche ${event.key}`);
-
-    }
+    // J'observe les keyup sur mon document
+    let keyups = Rx.Observable.fromEvent(document.body, 'keyup')
+        // Plutot que de filtrer sur tout l'event, je ne filtre que sur la
+        // propriete key
+        .pluck('key')
+        .filter(key => key === 'f' )
+        // Je m'abonne pour declencher des comportements
+        .subscribe(
+            () => requestFullScreen()
+        )
 
     function requestFullScreen() {
         if (document.documentElement.mozRequestFullScreen) {
